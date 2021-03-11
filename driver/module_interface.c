154a9e1fab6ce9f441e390e1193a60650ede9256
Andrea Bondavalli <andrea.bondavalli74@gmail.com>
2021-03-11 17:47:11 +0100

    Changed version to 1.1

--- driver/module_interface.c
+++ driver/module_interface.c
@@ -59,7 +59,7 @@
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Merging Technologies <alsa@merging.com>");
 MODULE_DESCRIPTION("Merging Technologies RAVENNA ALSA driver"); // see modinfo
-MODULE_VERSION("bondagit-1.1-beta");
+MODULE_VERSION("bondagit-1.1");
 MODULE_SUPPORTED_DEVICE("{{ALSA,Merging RAVENNA}}");
 
 
